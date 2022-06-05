354c5cb7d9c489bddda5ca5fc0f0138038823c60
Kelvin Ochubili <kelvinochubili@gmail.com>
2022-06-05 18:20:30 +0100

    import fix

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Footer from './Footer';
 import Sidebar from './Sidebar';
 import { ReactComponent as MenuIcon } from '../assets/svgs/menu.svg';
